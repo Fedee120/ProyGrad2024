c4d01de4ce4dd36573251a8f33a4a8641cdf69de
Fedee120 <federico.gil@fing.edu.uy>
2024-09-22 20:11:54 -0300

    version recontra inicial del agente

--- prototipo/agents/agent.py
+++ prototipo/agents/agent.py
@@ -1,31 +1,42 @@
-from langchain.agents import AgentExecutor, create_openai_tools_agent
+from langchain.agents import AgentExecutor, create_openai_tools_agent, create_tool_calling_agent
 from langchain_openai import OpenAI, ChatOpenAI
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.tools import Tool
 from tools.knowledge_base import KnowledgeBase
 from prompt.prompt_v2 import PROMPT
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Agent:
+    def __init__(self) -> None:
+        self.model = ChatOpenAI(model="gpt-4o")
+        self.tools = [KnowledgeBase()]
+        self.prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", "You are a helpful assistant"),
+                ("human", "{input}"),
+                # Placeholders fill up a **list** of messages
+                ("placeholder", "{agent_scratchpad}"),
+            ]
+        )
+        self.agent = self.create_agent()
+        self.agent_executor = self.create_agent_executor(self.agent)
+        pass
+
+    def create_agent(self):
+        return create_tool_calling_agent(self.model, self.tools, self.prompt)
+    
+    def create_agent_executor(self, agent):
+        return AgentExecutor(agent=agent, tools=self.tools)
+    
+    def invoke(self, message):
+        return self.agent_executor.invoke(message)
 
 
-# Initialize the GPT-4o-mini model
-model = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+agent = Agent()
+message = "Hola, quiero que me digas cuales son las implicaciones éticas de usar IA generativa en el aula. Soy un docente de secundaria sin mucha experiencia en IA, por lo que quiero una explicación simple pero completa."
+print(agent.agent_executor.invoke({"input": message}))
 
-# Use your existing KnowledgeBase tool
-tools = [KnowledgeBase()]
-
-system_prompt = PROMPT
-
-agent_executor = create_react_agent(model, tools, state_modifier=system_prompt)
-
-# Initialize the prompt template with placeholders for conversation history
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", PROMPT),
-        MessagesPlaceholder("chat_history", optional=True),
-        ("human", "{input}"),
-        MessagesPlaceholder("agent_scratchpad"),
-    ]
-)
-
-response = agent_executor.invoke({"messages": [("user", "Hola, quiero que me digas cuales son las implicaciones eticas de usar IA generativa en el aula. Soy un docente de secundaria sin mucha experiencia en IA, por lo que quiero una explicacion simple pero completa.")]})
-print(response["messages"])
