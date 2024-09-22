66323726b9316fbc71296f6fdf628a450b847b69
Fedee120 <federico.gil@fing.edu.uy>
2024-09-22 18:52:46 -0300

    se mueven los splitters

--- prototipo/ingestion/splitters/character_splitter.py
+++
@@ -1,6 +0,0 @@
-from langchain_text_splitters import CharacterTextSplitter
-
-def split_by_character(documents):
-    text_splitter = CharacterTextSplitter(separator='\n', chunk_size=1000, chunk_overlap=0)
-    chunks = text_splitter.split_documents(documents)
-    return chunks
