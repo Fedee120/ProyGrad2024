1a6bba2c00d18a7366fe53cf53c414e255a009fe
Fedee120 <federico.gil@fing.edu.uy>
2024-09-22 18:54:05 -0300

    se crea la tool que usa el rag

---
+++ prototipo/tools/knowledge_base.py
@@ -0,0 +1,42 @@
+from typing import Optional, Type
+
+from langchain_core.callbacks import (
+    CallbackManagerForToolRun,
+)
+from langchain_core.tools import BaseTool
+from pydantic import BaseModel, Field
+
+from rags.factory.RAGFactory import RAGFactory
+
+class KnowledgeBaseInput(BaseModel):
+    query: str = Field(description="query")
+    fallback: str = Field(description="Fallback answer")
+
+
+# Note: It's important that every field has type hints. BaseTool is a
+# Pydantic class and not having type hints can lead to unexpected behavior.
+class KnowledgeBase(BaseTool):
+    name: str = "Knowledge_Base"
+    description: str = "Útil para responder preguntas."
+    args_schema: Type[BaseModel] = KnowledgeBaseInput
+    return_direct: bool = True
+
+    def _run(
+        self, query: str, fallback: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool."""
+        rag = RAGFactory.create_rag(
+            URI="http://localhost:19530", 
+            COLLECTION_NAME="real_collection", 
+            search_kwargs={"k": 10}, 
+            search_type="mmr", 
+            llm_model_name="gpt-4o-mini", 
+            embeddings_model_name="text-embedding-3-small")
+        return rag.generate_answer(query)
+    
+def create_tool():
+    return KnowledgeBase()
+
+if __name__ == "__main__":
+    tool = create_tool()
+    print(tool.run({"query": "¿Cuál es el color del cielo?", "fallback": "No sé"}).get("answer"))
