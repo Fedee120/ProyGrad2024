033e72beb4524eff742aaa8dd3b4bce8c03951a0
William Tift <williamtift@outlook.com>
2024-11-17 00:02:34 -0300

    Fix milvus gRPC warning due to not closed connection

--- backend/agent/knowledge_base.py
+++ backend/agent/knowledge_base.py
@@ -4,18 +4,16 @@ import os
 from rags.factory.RAGFactory import RAGFactory
 
 class KnowledgeBase():
-    def __init__(self):
+    def search(self, query: str):
         load_dotenv()
-        self.rag = RAGFactory.create_rag(
+        rag = RAGFactory.create_rag(
             URI=os.getenv("MILVUS_STANDALONE_URL"), 
             COLLECTION_NAME="real_collection", 
             search_kwargs={"k": 10}, 
             search_type="mmr", 
             llm_model_name="gpt-4o", 
             embeddings_model_name="text-embedding-3-small")
-        
-    def search(self, query: str):
-        return self.rag.generate_answer(query)
+        return rag.generate_answer(query)
 
 if __name__ == "__main__":
     tool = KnowledgeBase()
